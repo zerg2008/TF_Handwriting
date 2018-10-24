d9523cb7ff02754a61594c83e08e125b848e68ab
zerg2008 <42507592@qq.com>
2018-10-24 10:20:22 +0800

    first commit

--- run.py
+++ run.py
@@ -2,8 +2,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 import numpy as np
 import Image
-#zerg，20181024 test
-
+#zerg，20181024 
 def weight_variable(shape, dtype, name):
     initial = tf.truncated_normal(shape = shape, stddev = 0.1, dtype = dtype, name = name)
     return tf.Variable(initial)
